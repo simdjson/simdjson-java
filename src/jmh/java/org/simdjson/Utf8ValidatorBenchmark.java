82d542cf7fe8d747a33e82a87a9c92b0713d43a3
nostimo <rebirth.shc@gmail.com>
2023-08-19 19:16:03 +0100

    store benchmark state directly in a  class

--- src/jmh/java/org/simdjson/Utf8ValidatorBenchmark.java
+++ src/jmh/java/org/simdjson/Utf8ValidatorBenchmark.java
@@ -1,39 +1,34 @@
 package org.simdjson;
 
+import com.google.common.base.Utf8;
 import org.openjdk.jmh.annotations.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
-import com.google.common.base.Utf8;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class Utf8ValidatorBenchmark {
-    @State(Scope.Benchmark)
-    public static class MyState {
-        @Param({"/twitter.json", "/gsoc-2018.json", "/github_events.json"})
-        String fileName;
-        byte[] bytes;
+    @Param({"/twitter.json", "/gsoc-2018.json", "/github_events.json"})
+    String fileName;
+    byte[] bytes;
 
-        @Setup(Level.Trial)
-        public void doSetup() {
-            try (InputStream is = Utf8ValidatorBenchmark.class.getResourceAsStream(fileName)){
-                bytes = is.readAllBytes();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    @Setup(Level.Trial)
+    public void setup() throws IOException {
+        try (InputStream is = Utf8ValidatorBenchmark.class.getResourceAsStream(fileName)) {
+            bytes = is.readAllBytes();
         }
     }
 
     @Benchmark
-    public void utf8Validator(MyState myState)  {
-        Utf8Validator.validate(myState.bytes);
+    public void utf8Validator() {
+        Utf8Validator.validate(bytes);
     }
 
     @Benchmark
-    public boolean guava(MyState myState) {
-        return Utf8.isWellFormed(myState.bytes);
+    public boolean guava() {
+        return Utf8.isWellFormed(bytes);
     }
 }
